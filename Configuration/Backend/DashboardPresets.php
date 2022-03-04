d3632cd205c96a5259e7561be001d4dcb6f864ad
Karsten Nowak <nowak@undkonsorten.com>
2022-03-04 08:43:51 +0100

    [TASK] Remove some luxletter stuff
    Related: #21420

--- Configuration/Backend/DashboardPresets.php
+++ Configuration/Backend/DashboardPresets.php
@@ -2,9 +2,9 @@
 return [
     'luxletter' => [
         'title' =>
-            'LLL:EXT:luxletter/Resources/Private/Language/locallang_db.xlf:module.dashboard.preset.luxletter.title',
+            'LLL:EXT:cute_mailing/Resources/Private/Language/locallang_db.xlf:module.dashboard.preset.luxletter.title',
         'description' =>
-            'LLL:EXT:luxletter/Resources/Private/Language/locallang_db.xlf:module.dashboard.preset.luxletter.description',
+            'LLL:EXT:cute_mailing/Resources/Private/Language/locallang_db.xlf:module.dashboard.preset.luxletter.description',
         'iconIdentifier' => 'extension-luxletter',
         'defaultWidgets' => [
             'luxletterReceiver',
--- Configuration/Backend/DashboardWidgetGroups.php
+++ Configuration/Backend/DashboardWidgetGroups.php
@@ -1,6 +1,6 @@
 <?php
 return [
     'luxletter' => [
-        'title' => 'LLL:EXT:luxletter/Resources/Private/Language/locallang_db.xlf:module.dashboard.group.luxletter.title'
+        'title' => 'LLL:EXT:cute_mailing/Resources/Private/Language/locallang_db.xlf:module.dashboard.group.luxletter.title'
     ]
 ];
