0c9ce044a6280a660f42afe09d18faefb90b82b3
Karsten Nowak <nowak@undkonsorten.com>
2024-10-04 16:38:26 +0200

    [TASK] Add 2 forgotten files in TCA/Overrides
    Probably they are added by rector before.

---
+++ Configuration/TCA/Overrides/tx_cutemailing_domain_model_newsletter.php
@@ -0,0 +1,3 @@
+<?php
+
+$GLOBALS['TCA']['tx_cutemailing_domain_model_newsletter']['ctrl']['security']['ignorePageTypeRestriction'] = true;
---
+++ Configuration/TCA/Overrides/tx_cutemailing_domain_model_recipient_list.php
@@ -0,0 +1,3 @@
+<?php
+
+$GLOBALS['TCA']['tx_cutemailing_domain_model_recipient_list']['ctrl']['security']['ignorePageTypeRestriction'] = true;
