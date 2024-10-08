aaf3184bf38958e257d454d05a77c0fde4f94b54
Karsten Nowak <nowak@undkonsorten.com>
2024-10-08 16:11:20 +0200

    [TASK] Remove icon registry from localconf
    Already registered in `Configuration/Icons.php`. This is the default.
    
    Related: #26439

--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -1,8 +1,10 @@
 <?php
 
+use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
+
 return [
     'apps-pagetree-cute-mailing' => [
-        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'provider' => SvgIconProvider::class,
         'source' => 'EXT:cute_mailing/Resources/Public/Icons/cute_folder_icon.svg',
     ],
 ];
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,6 +1,5 @@
 <?php
 
-use TYPO3\CMS\Core\Imaging\IconRegistry;
 use TYPO3\CMS\Core\Information\Typo3Version;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
@@ -11,23 +10,12 @@ call_user_func(
     function ($extKey = 'cute_mailing') {
         $typo3VersionInformation = GeneralUtility::makeInstance(Typo3Version::class);
 
-        // Provide icon for page tree, list view, ... :
-        $iconRegistry = GeneralUtility::makeInstance(IconRegistry::class);
-        $iconRegistry
-            ->registerIcon(
-                'apps-pagetree-cute-mailing',
-                TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-                [
-                    'source' => 'EXT:' . $extKey . '/Resources/Public/Icons/cute_folder_icon.svg',
-                ]
-            );
-
         $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update']['cuteMailing_folderUpdateWizard']
             = \Undkonsorten\CuteMailing\Updates\FolderUpdateWizard::class;
 
         $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['cute_mailing'] ??= [];
         $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['cute_mailing']['backend'] ??= \TYPO3\CMS\Core\Cache\Backend\Typo3DatabaseBackend::class;
-        
+
         if ( $typo3VersionInformation->getMajorVersion() < 13) {
             $GLOBALS['TYPO3_CONF_VARS']['FE']['addRootLineFields'] = $GLOBALS['TYPO3_CONF_VARS']['FE']['addRootLineFields'].', module';
         }
