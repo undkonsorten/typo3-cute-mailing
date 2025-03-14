668d97f5662d4a690369c6f4b16f7d7d77f891b5
Eike Starkmann <eike.starkmann@undkonsorten.com>
2025-03-14 11:30:05 +0100

    [TASK] add ext_emconf.php for TER

---
+++ ext_emconf.php
@@ -0,0 +1,21 @@
+<?php
+
+/** @noinspection PhpUndefinedVariableInspection */
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Cute mailing',
+    'description' => 'A TYPO3 extension that makes mailing and newsletter sending cute. Send TYPO3 pages as emails (newsletter) to many recipients. Can be used as replacement for direct_mail.',
+    'category' => 'module',
+    'author' => 'Eike Starkmann',
+    'author_email' => 'starkmann@undkonsorten.com',
+    'state' => 'stable',
+    'version' => '4.1.1',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '12.4.0-13.99.99',
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [
+        ],
+    ],
+];
